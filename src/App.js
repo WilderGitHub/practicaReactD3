b30d29e080b0c16cf8ce3042abcc17f8f4c18354
WilderGitHub <wilder_serdan@hotmail.com>
2021-01-26 13:41:57 -0400

    primera practica en bruto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState,useEffect } from 'react';
+import { csv,max,scaleBand,scaleLinear } from 'd3';
+/* import {max} from 'd3-array'; */
 import './App.css';
+ 
+const csvURL = 'https://raw.githubusercontent.com/WilderGitHub/csvs/main/datos.csv';
 
-function App() {
+const App = () => {
+  const [data, setData]= useState(null);
+  useEffect(() => {
+    //csv(csvURL).then(data=>{setData(data)})  Esto es equivalente a la linea de abajo
+    csv(csvURL).then(setData) 
+    
+  }, []);
+   if(!data){
+    
+    return <pre>Cargando..</pre>
+    
+  } 
+  //console.log(data);
+  //console.log("maximo");
+  //console.log(max(data,d=>d.poblacion));  
+  const escalaAncho = scaleLinear().domain([0,max(data,d=>d.poblacion)]).range([0,400]);
+  const escalaY = scaleBand().domain(data.map(d=>d.pais)).range([0,200]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <svg width='500' height='500'>
+        {data.map((d,i)=>
+        <text x="10" y={escalaY(d.pais)} fill="red">{(i+1) + d.pais}</text>
+        )}
+        {data.map((d,i)=>
+        <rect x="90" y={escalaY(d.pais)} width={escalaAncho(d.poblacion)} height={escalaY.bandwidth()/2} />
+        
+        )}
+                
+      </svg>
+  )
 }
+export default App
 
-export default App;
